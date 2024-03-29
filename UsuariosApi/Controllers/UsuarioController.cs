7060cdcf89fb5b1c25ebf13470faa1cb338e37fa
heroshg <heros.goodoy@gmail.com>
2024-03-29 15:07:34 -0300

    Usuarios-api

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class UsuarioController : ControllerBase
+{
+    private UsuarioService _usuarioService;
+
+    public UsuarioController(UsuarioService cadastroService)
+    {
+        _usuarioService = cadastroService;
+    }
+
+    [HttpPost("cadastro")]
+    public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
+    {
+        await _usuarioService.CadastraUsuario(dto);
+        return Ok("Usuário cadastrado!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginUsuarioDto dto)
+    {
+        var token = await _usuarioService.LoginAsync(dto);
+        return Ok(token);
+    }
+}
